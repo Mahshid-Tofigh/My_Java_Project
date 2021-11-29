a12e5c906c24f66ad2651d608b5665c8a6a227b6
Mahshid-Tofigh <94631128+Mahshid-Tofigh@users.noreply.github.com>
2021-11-29 10:42:02 -0500

    Add files via upload

--- New_hired_employee/New_hired.java
+++ New_hired_employee/New_hired.java
@@ -1,1 +1,9 @@
+public class New_hired {
+    public static void main(String[] args) {
+        Email employee1 = new Email("Sarah","Smith", "Magnolia");
+        System.out.println(employee1.user_info());
+        employee1.setAlternate_email("Alternate email is: s.smith@gamil.com");
+        System.out.println(employee1.getAlternate_email());
 
+    }
+}
